8f1f7d80a9dcc6d3da92d3f887fb4326705066cf
thuatdt137 <dothuat098@gmail.com>
2024-11-07 19:37:44 +0700

    first commit
    first commit

---
+++ src/test/java/vn/fsaproject/carental/CarentalApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.fsaproject.carental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
